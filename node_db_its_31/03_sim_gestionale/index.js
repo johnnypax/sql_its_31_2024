2058205ebef5b8799c8c4ebbfb61856c17b805a5
Giovanni Pace <g.pace@braintechinnovations.it>
2025-03-31 11:45:03 +0200

    Aggiunta gestione API

--- node_db_its_31/03_sim_gestionale/index.js
+++ node_db_its_31/03_sim_gestionale/index.js
@@ -3,6 +3,9 @@ const mysql = require('mysql2');
 
 const app = express();                  //Inizializzo il server Express
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
 const port = 4001;
 const host = "127.0.0.1"
 const connessione = mysql.createConnection(
@@ -17,12 +20,69 @@ const connessione = mysql.createConnection(
 
 
 app.listen(port, host, () => {
-    console.log(`Sono in ascolto all'indirizzo: http://${host}:${port}`);  
-    
+    console.log(`Sono in ascolto all'indirizzo: http://${host}:${port}`);
+
     connessione.connect(errore => {
-        if(errore)
-            console.log("Errore di connessione ;(");
+        if (errore)
+            console.log("Errore di connessione al database ;(");
         else
-            console.log("Sei connesso con successo! xD")
+            console.log("Sei connesso con successo al database xD")
     });
 })
+
+//Restituisci tutte le SIM
+app.get("/sim", (req, res) => {
+    let query = "SELECT simID, codice, telefono, operatore, credito, nominativo FROM Sim;";
+
+    connessione.query(query, (errore, risultato) => {
+        if (errore)
+            res.status(500).json(errore);
+        else
+            res.status(200).json(risultato)
+    })
+})
+
+//Restituisci la sim con ID non predefinito, es... ../sim/3
+app.get("/sim/:id", (req, res) => {
+    let simID = req.params.id;
+
+    let query = `SELECT simID, codice, telefono, operatore, credito, nominativo 
+                    FROM Sim 
+                    WHERE simID = ${simID}`;
+
+    connessione.query(query, (errore, risultato) => {
+        if (errore)
+            res.status(500).json(errore);
+        else{
+            let elemento = risultato[0];
+
+            if(!elemento)
+                res.status(404).json();
+            else
+                res.status(200).json(elemento)
+                        
+        }
+            
+    })
+})
+
+app.post("/sim", (req, res) => {
+    let nuovaSim = req.body;
+
+    let sql = `INSERT INTO Sim (codice, telefono, operatore, credito, nominativo) VALUES
+                ('${nuovaSim.codice}', '${nuovaSim.telefono}', '${nuovaSim.operatore}', 
+                ${nuovaSim.credito}, '${nuovaSim.nominativo}');`;
+
+    connessione.query(sql, (errore, risultato) => {
+        if(errore)
+            res.status(500).json(errore.sqlMessage);
+        else{
+            if(risultato.affectedRows > 0)
+                res.status(201).json();
+            else
+                res.status(400).json();
+        }
+            
+    })
+})
+
